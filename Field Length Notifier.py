1b550a7beae9bbeb471af8a0b981e91dfe3b5102
Dharmik Babariya <81213533+justdharmik@users.noreply.github.com>
2021-03-24 09:35:20 +0530

    Delete Field Length Notifier.py

--- Field Length Notifier.py
+++
@@ -1,9 +0,0 @@
-#******FIELD LENGTH CALC******
-
-name = input('Enter your name : ')
-if len(name) < 3:
-    print('Name must be at east 3 characters long!')
-elif len(name) > 25:
-    print('Name must be less than 15 characters!ss')
-else:
-    print('Name looks good!')
