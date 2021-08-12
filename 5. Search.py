9deb9e49d6f9ec4df5b740bdb198e52a91c60902
Dharmik Babariya <81213533+justdharmik@users.noreply.github.com>
2021-08-12 11:40:16 +0530

    Update 5. Search.py

--- 5. Search.py
+++ 5. Search.py
@@ -1,13 +1,18 @@
-def search(text, word):
-    if word in text:
-        result = f'Word found! "{word}" in "{text}"'
-    else:
-        result = 'Word not found'
+def search(text, word, count):
+    lst = list(text.split(" "))
+    for i in lst:
+        if i == word:
+            result = f'Word found! " {word}" in " {text}"'
+            count = count + 1
+        else:
+            result = 'Word not found'
+    print(f'Instances: {count}')
     return result
 
 #text = input("Enter the text: ")
 #word = input("Enter the word: ")
-text = "Hello There"
+text = "Hello There Hello"
 word = "Hello"
-
-print(search(text, word))
+count = 0
+lst = []
+print(search(text, word, count))
